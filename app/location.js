a135630db2320b2492b624e73f36640ebfcca87f
Samuel <s.okoro07@gmail.com>
2020-11-02 10:56:15 +0100

    absolute coordinates on map

--- app/location.js
+++ app/location.js
@@ -3,9 +3,12 @@ import {View, TouchableOpacity, Image, Text} from 'react-native'
 
 
 const LOCATION_SIZE = 60;
+var coordinates = {x:0,y:0};
 
-const Location = () => {
-		return(<View style={{backgroundColor:'#333', width:LOCATION_SIZE, height:LOCATION_SIZE, borderRadius:LOCATION_SIZE/2}}>
+const Location = (props) => {
+	//coordinates = props.useCoordinate(0)[0]
+	console.log(props)
+		return(<View style={{position:'absolute', left:props.locations.item.x, bottom:props.locations.item.y, backgroundColor:'#333', width:LOCATION_SIZE, height:LOCATION_SIZE, borderRadius:LOCATION_SIZE/2}}>
 			<TouchableOpacity stlye={{flex:1}}>
 			</TouchableOpacity>
 			</View>)
--- app/map.js
+++ app/map.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import {View, TouchableOpacity, Text} from 'react-native'
+import {View,FlatList, Dimensions, TouchableOpacity, Text} from 'react-native'
 import { connect } from 'react-redux';
 import Location from './location'
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const OSET = windowWidth/5;
  
+const locationMap = [{
+	x: OSET,
+	y: 100
+},{
+	x: -30 - OSET,
+	y: 0
+},{
+	x: -50 + OSET,
+	y: 0 - OSET
+},
+{
+	x: -30,
+	y: -2.5*(OSET),
+},{
+	x: -130,
+	y: 2*(OSET),
+}
+]
+
+const useCoordinate = (coordinate) => {
+	const locationVal = locationMap.splice(coordinate, 1)
+	//console.log(locationMap)
+	return locationVal;
+}
 
 const map = (props) => {
   return (
-    <View stlye = {{flex:1}}>
-    <Location />
+    <View style = {{flex:1, justifyContent:'flex-end', paddingTop:20}}>
+    <FlatList
+    style = {{alignSelf:'center', backgroundColor:'#333'}}
+    contentContainerStyle= {{alignItems:'center', justifyContent: 'center',flex: 1, backgroundColor:'#ddd'}}
+      bounce = {false}
+      style = {{marginBottom:30 }}
+      data = {locationMap}
+      keyExtractor={item => item.x}
+      renderItem = {(items) => <Location locations = {items}/>} />
     <Text>{props.game.day}</Text>
     </View>
   )
