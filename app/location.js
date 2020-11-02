d33e7e81d95d854b149cc9768001ee7ad3c75d4a
Samuel <s.okoro07@gmail.com>
2020-11-02 12:25:41 +0100

    working store image

--- app/location.js
+++ app/location.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {View, TouchableOpacity, Image, Text} from 'react-native' 
-
+import {View, TouchableOpacity, Image, ImageBackground, Text} from 'react-native' 
+import store from './images/store.png'
 
 const LOCATION_SIZE = 60;
 var coordinates = {x:0,y:0};
@@ -8,13 +8,13 @@ var coordinates = {x:0,y:0};
 const Location = (props) => {
 	//coordinates = props.useCoordinate(0)[0]
 	console.log(props)
-		return(<View style={{position:'absolute',shadowColor: '#000',
+		return(<TouchableOpacity  style={{position:'absolute',shadowColor: '#000',
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.8,
+    overflow: 'hidden',
     shadowRadius: 1, elevation:5, left:props.locations.item.x, bottom:props.locations.item.y, backgroundColor:'#ccc', width:LOCATION_SIZE, height:LOCATION_SIZE, borderRadius:LOCATION_SIZE/2}}>
-			<TouchableOpacity stlye={{flex:1}}>
-			</TouchableOpacity>
-			</View>)
+			<Image source={store} resizeMode = "stretch" style={{flex:1, height:LOCATION_SIZE, width:LOCATION_SIZE}} />
+			</TouchableOpacity>)
 	}
 
 export default Location;
--- app/map.js
+++ app/map.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {View,FlatList, Dimensions, TouchableOpacity, Text} from 'react-native'
+import {View,FlatList, ImageBackground, Dimensions, TouchableOpacity, Text} from 'react-native'
 import { connect } from 'react-redux';
 import Location from './location'
+import map1 from './images/map1.jpg'
+import store from './images/store.png'
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
@@ -9,7 +11,8 @@ const OSET = windowWidth/5;
  
 const locationMap = [{
 	x: OSET,
-	y: 100
+	y: 100,
+	img: store,
 },{
 	x: -30 - OSET,
 	y: 0
@@ -34,17 +37,19 @@ const useCoordinate = (coordinate) => {
 
 const map = (props) => {
   return (
-    <View style = {{flex:1, justifyContent:'flex-end', borderRadius:10, padding:20}}>
+    <ImageBackground source={map1} style = {{flex:1, width:null, height:null, justifyContent:'flex-end', borderRadius:10, padding:20}}>
     <FlatList
     style = {{alignSelf:'center', backgroundColor:'#333'}}
-    contentContainerStyle= {{alignItems:'center', borderRadius:10, justifyContent: 'center',flex: 1, backgroundColor:'#ddd'}}
+    contentContainerStyle= {{alignItems:'center', borderRadius:10, justifyContent: 'center',flex: 1, backgroundColor:'rgba(255,255,255,0.4)'}}
       bounce = {false}
       style = {{marginBottom:10 }}
       data = {locationMap}
       keyExtractor={item => item.x}
       renderItem = {(items) => <Location locations = {items}/>} />
+    <View style={{backgroundColor:'#c0a47c', padding:10}}>
     <Text style={{alignSelf:'center'}}>{props.game.day} days have past</Text>
     </View>
+    </ImageBackground>
   )
 }
 
